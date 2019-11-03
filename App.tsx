7d3cc06e9b0b4f6a4987fb360c65b65ad5b1f593
polnikale <nikpolale@gmail.com>
2019-11-03 23:20:03 +0200

    Basic graphql setup + fix crash

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
+import { ApolloProvider } from '@apollo/react-hooks';
 import React from 'react';
 import Root from './src/Root';
+import client from './src/store';
 
 const App = () => {
-  return <Root />;
+  return (
+    <ApolloProvider client={client}>
+      <Root />
+    </ApolloProvider>
+  );
 };
 
 export default App;
